9fec92559e1f59f74e3ec335f4d3dae2ea97557c
Maksym <106184045+Makzumm@users.noreply.github.com>
2024-03-09 18:03:38 +0100

    commit #2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-
+import { nanoid } from 'nanoid'
 
 const form = document.querySelector("#task-form");
+const taskEl = document.querySelector('#task-list')
+
 
 form.addEventListener("submit", onTaskSubmit);
 
 function onTaskSubmit(event) {
     event.preventDefault();
     const inputValue = event.currentTarget.elements.taskName.value.trim();
-    if(!inputValue) return;
+    if (!inputValue) return;
+    createListEl(inputValue);
     console.log(inputValue);
     event.currentTarget.reset();
+}
+
+function createListEl(textValue) {
+    const id = nanoid();
+    taskEl.insertAdjacentHTML("beforeend", `<li id=${id}>${textValue}<button type='button'>x</button></li>`)
 }
