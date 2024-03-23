dbc883befb0db0240dc455df38beb5a75fb17831
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-03-23 19:34:39 +0000

    Deploying to gh-pages from @ KaterynaKocheharova/practise-vite-101@d4677e4c1c5d81ed8bc3f514fc7154edeade82bf 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{l as n,a as e,T as a,r as o}from"./assets/dataMarkup-d11a45c6.js";n({TASK_KEY:a,container:o.favoriteList});e(o.favoriteList,a);function t(r){return function(s){console.log(10),console.log(`Person ${r}${s}`)}}const c=t(18);c(18);t("Katya")(18);
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{l as n,a as e,T as o,r as a}from"./assets/dataMarkup-2975490a.js";n({TASK_KEY:o,container:a.favoriteList});e(a.favoriteList,o);function t(r){return function(s){console.log(10),console.log(`Person ${r}${s}`)}}const i=t(18);i(18);t("Katya")(18);
 //# sourceMappingURL=commonHelpers2.js.map
