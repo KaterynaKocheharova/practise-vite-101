5778bd0cced4937a98005df535b535c5abb34ed4
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-03-23 17:08:57 +0000

    Deploying to gh-pages from @ KaterynaKocheharova/practise-vite-101@17edc14573d40e39c5d9f40c872668bc1b8d704e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{g as a,l as o,T as l}from"./assets/dataMarkup-2b294bfd.js";console.log(a(l));o();
+import{l as n,a as e,T as a,r as o}from"./assets/dataMarkup-d11a45c6.js";n({TASK_KEY:a,container:o.favoriteList});e(o.favoriteList,a);function t(r){return function(s){console.log(10),console.log(`Person ${r}${s}`)}}const c=t(18);c(18);t("Katya")(18);
 //# sourceMappingURL=commonHelpers2.js.map
